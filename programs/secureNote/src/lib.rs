c5ca55ecdaa59eb861fa86ac2ca61fd2f73473d7
Tim Schmidt <tim@neodyme.io>
2022-11-23 21:26:10 +0100

    exclude security_txt from no-entrypoint builds

--- programs/secureNote/src/lib.rs
+++ programs/secureNote/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 use solana_security_txt::security_txt;
 
+#[cfg(not(feature = "no-entrypoint"))]
 security_txt! {
     // Required fields
     name: "Me::Anon::0xdeep",
